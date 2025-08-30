3b9696096dd8650d7829e782726d62c658d1ed2f
hosein-gh <m.ghelichkhani@ebidar.net>
2025-08-30 09:54:09 +0330

    update project

--- linq_pagination_faster_method/Program.cs
+++ linq_pagination_faster_method/Program.cs
@@ -13,49 +13,54 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
 app.MapGet("/linq/{one:int}-{two:int}", (int one, int two) =>
     {
-        var bigdata = Moq.Get(); 
-        
+        var bigdata = Moq.Get();
+
         var paginationOld = Moq.GetOld(one, two);
         var paginationNew = Moq.GetNew(one, two);
-        
-        return Results.Ok(new {bigdata,paginationOld, paginationNew});
+
+        return Results.Ok(new { bigdata, paginationOld, paginationNew });
     })
     .WithName("linq")
     .WithOpenApi();
 
 app.Run();
 
-public class Moq
+internal static class Moq
 {
     public static List<Sample> Get()
-        =>  Enumerable
-        .Range(0, 100)
-        .Select( o
-            => new Sample(Id:o,Name:o.ToString()))
-        .ToList();
-
-    public static List<Sample> GetOld(int pageNumber, int pageSize)
-        => Enumerable
+    {
+        return Enumerable
             .Range(0, 100)
             .Select(o
-                => new Sample(Id: o, Name: o.ToString()))
+                => new Sample(o, o.ToString()))
+            .ToList();
+    }
+
+    public static List<Sample> GetOld(int pageNumber, int pageSize)
+    {
+        return Enumerable
+            .Range(0, 100)
+            .Select(o
+                => new Sample(o, o.ToString()))
             .OrderBy(o => o.Id)
             .Skip(pageNumber * pageSize)
             .Take(pageSize)
-        .ToList();
+            .ToList();
+    }
 
     public static List<Sample> GetNew(int lastId, int pageSize)
-        => Enumerable
+    {
+        return Enumerable
             .Range(0, 100)
             .Select(o
-                => new Sample(Id: o, Name: o.ToString()))
+                => new Sample(o, o.ToString()))
             .OrderBy(o => o.Id)
             .Where(o => o.Id > lastId)
             .Take(pageSize)
             .ToList();
+    }
 }
 
-public record Sample(int Id,string Name);
+internal record Sample(int Id, string Name);
